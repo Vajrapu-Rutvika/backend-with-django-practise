efa69fa49e74e47438d5214d75edff0db072a3fe
Vajrapu Rutvika <vajrapurutvika@gmail.com>
2026-08-10 19:15:15 +0530

    created a small model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    views=models.IntegerField(default=0)
+    published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
