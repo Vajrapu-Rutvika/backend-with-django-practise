3949a5e0670eba14337e2c759af389b81672c71f
Vajrapu Rutvika <vajrapurutvika@gmail.com>
2026-08-27 18:47:15 +0530

    password reset

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -23,5 +24,10 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name="dashboard"),
     path('pass_change/',views.reset_pass,name="reset_pass"),
 
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name="password_reset"),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+ 
 
 ]
