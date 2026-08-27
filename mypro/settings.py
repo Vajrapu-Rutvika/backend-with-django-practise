4b9d2abf6d02c986842075a1d35058c4878dbd6e
Vajrapu Rutvika <vajrapurutvika@gmail.com>
2026-08-27 19:19:24 +0530

    password reset via email

--- mypro/settings.py
+++ mypro/settings.py
@@ -43,7 +43,13 @@ INSTALLED_APPS = [
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
-EMAIL_BACKEND="django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+
+EMAIL_HOST_USER='vajrapurutvika@gmail.com'
+EMAIL_HOST_PASSWORD='lxth kpvo vvzs onib'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
